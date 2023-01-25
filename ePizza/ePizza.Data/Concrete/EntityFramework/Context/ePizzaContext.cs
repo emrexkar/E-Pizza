4a7d8ad37b03c269dfdba23f41294756625c3c2d
Semih Özcan <109103403+semihozcann@users.noreply.github.com>
2023-01-25 18:45:34 +0300

    Add files via upload

---
+++ ePizza/ePizza.Data/Concrete/EntityFramework/Context/ePizzaContext.cs
@@ -0,0 +1,16 @@
+﻿
+using ePizza.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePizza.Data.Concrete.EntityFramework.Context
+{
+    public class ePizzaContext : IdentityDbContext<User, Role, int, UserClaim,UserRole,UserLogin,UserToken, RoleClaim>
+    {
+    }
+}
