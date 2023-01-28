1447e5635207c017a393d8013c84d69728328d19
Semih Özcan <109103403+semihozcann@users.noreply.github.com>
2023-01-28 09:58:29 +0300

    Add files via upload

--- ePizza/ePizza.Data/Concrete/EntityFramework/Context/ePizzaContext.cs
+++ ePizza/ePizza.Data/Concrete/EntityFramework/Context/ePizzaContext.cs
@@ -1,7 +1,9 @@
 ﻿
+using ePizza.Data.Concrete.EntityFramework.Mappings;
 using ePizza.Entities.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,51 @@ using System.Threading.Tasks;
 
 namespace ePizza.Data.Concrete.EntityFramework.Context
 {
-    public class ePizzaContext : IdentityDbContext<User, Role, int, UserClaim,UserRole,UserLogin,UserToken, RoleClaim>
+    public class ePizzaContext : IdentityDbContext<User, Role, int, UserClaim,UserRole,UserLogin,RoleClaim, UserToken>
     {
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
+        public ePizzaContext()
+        {
+
+        }
+
+
+        //configuration ayarları olacak.
+        public ePizzaContext(DbContextOptions<ePizzaContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        //Db kurallarını burada db oluşurken veriyoruz.
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new PaymentDetailsMap());
+            builder.ApplyConfiguration(new CartMap());
+            //builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new ProductTypeMap());
+            builder.ApplyConfiguration(new CategoryMap());
+            builder.ApplyConfiguration(new AddressMap());
+            builder.ApplyConfiguration(new OrderMap());
+            builder.ApplyConfiguration(new OrderItemMap());
+            //builder.ApplyConfiguration(new RoleMap());
+            builder.ApplyConfiguration(new CartMap());
+
+        }
     }
 }
